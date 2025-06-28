fadc06cb815e45a280ff8eaf891826e8c33b1469
JovanyVale <gambino84_@hotmail.com>
2025-06-27 19:54:16 -0700

    Membership section

--- app/page.tsx
+++ app/page.tsx
@@ -88,25 +88,32 @@ export default function Home() {
       </div>
 
       {/* Trending Products */}
-      <div className="bg-black pt-12 flex flex-col items-center">
-        <h1>TRENDING PRODUCTS</h1>
-        <div className="grid lg:grid-cols-3 sm:grid-cols-2 place-items-center w-full lg:w-11/12 my-12">
-          {products.slice(0, 6).map((product: Data) => (
-            <Product
-              key={product.id}
-              name={product.name}
-              price={product.price}
-              description={product.description}
-              category={product.category}
-              image={product.image}
-              stock={product.stock}
-            />
-          ))}
-        </div>
+      <div className="bg-neutral-950 py-20 flex flex-col items-center">
+        <h1 className="text-lg">TRENDING PRODUCTS</h1>
+
+        {
+          productsStatus == 200
+            ? <div className="grid lg:grid-cols-3 sm:grid-cols-2 place-items-center w-full lg:w-11/12 my-12">
+              {products.slice(0, 6).map((product: Data) => (
+                <Product
+                  key={product.id}
+                  name={product.name}
+                  price={product.price}
+                  description={product.description}
+                  category={product.category}
+                  image={product.image}
+                  stock={product.stock}
+                />))}
+            </div>
+
+            : <div className="flex flex-col justify-items-center place-content-center lg:col-start-2">
+              <p className="w-full place-self-center text-center loadingText text-xl md:my-70 my-52">Loading products...</p>
+            </div>
+        }
 
         {/* Delivery Banner */}
-        <div className="relative w-full md:h-[450px] h-[310px] border-y-3 border-white">
-          <div className="absolute z-20 flex flex-col h-full justify-center ml-[5%] md:ml-[12%]">
+        <div className="relative w-[92%] md:h-[450px] h-[350px] border-2 border-neutral-300 z-30 rounded-3xl">
+          <div className="absolute z-20 flex flex-col h-full justify-center ml-[5%] mr-[5%] md:mr-0 md:ml-[12%]">
             <h2 className="font-bold text-2xl md:text-4xl text-blue-300">Free delivery on <span className="text-white">$99+</span> orders</h2>
             <p className="text-md md:text-xl font-semibold mb-2">Get advantage of our quick delivery to your door!</p>
             <ul>
@@ -116,28 +123,47 @@ export default function Home() {
             </ul>
           </div>
 
-          {/* Catalog Banner */}
           <div>
             <Image src={"/images/banners/deliveryBanner.png"}
               alt={"deliveryBanner"}
               fill
-              className="object-right object-cover "
+              className="object-right object-cover rounded-3xl"
             />
           </div>
-          <div className="h-full w-full bg-black/50 z-10 absolute"></div>
+          <div className="h-full w-full bg-black/30 z-10 absolute"></div>
         </div>
       </div>
 
-      <div className="h-[500px] bg-[url('/images/background/catalogBanner.png')] bg-fixed bg-center bg-cover flex  flex-col justify-center place-items-center">
-        <div className="flex flex-col items-center w-[50%] gap-3">
-          <h1 className="text-7xl font-bold text-blue-300" >Catalog</h1>
-          <p className="font-bold text-xl text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus, similique eius distinctio illo, minus, hic quae illum inventore iste recusandae optio rem sit aliquam magni expedita modi quas consectetur tempore</p>
-          <button className="bg-white p-4 rounded-3xl text-black font-bold">See the whole catalog</button>
+      {/* Catalog Banner */}
+      <div className="h-[570px] bg-[url('/images/background/catalogBanner.png')] bg-fixed bg-center bg-cover flex  flex-col justify-center place-items-center">
+        <div className="flex flex-col items-center lg:w-[50%] w-[85%] gap-4 z-20">
+          <h1 className="md:text-7xl text-5xl font-bold text-blue-300" >Catalog</h1>
+          <p className="font-bold md:text-xl text-center">Check out our tech picks!
+            From cool gadgets to must-have gear.</p>
+          <button className="bg-neutral-800 p-4 rounded-3xl text-white font-bold hover:cursor-pointer shadow-black/70 shadow-lg">See the whole catalog</button>
+        </div>
+        <div className="h-[570px] w-full bg-black/60 absolute"></div>
+      </div>
+
+      {/** Members Club Banner */}
+      <div className="flex flex-col lg:w-[75%] w-[90%] items-center justify-center relative overflow-hidden mx-auto mt-12 rounded-3xl">
+        <div>
+          <div className="absolute">
+            <h2>Members <span className="">Club</span></h2>
+          </div>
+          <Image src={"/images/banners/memberClubBanner.png"}
+            alt="Member club banner"
+            height={750}
+            width={1600}
+            className="object-cover"
+          />
+        </div>
+
+        <div className="lg:h-26 h-12 w-full bg-neutral-600">
+          <p>Hola</p>
         </div>
       </div>
 
-
-
-    </main>
+    </main >
   );
 }
